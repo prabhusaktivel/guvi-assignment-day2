e120ca83270323ca253ec1c74657af08046a62e8
prabhusaktivel <92292429+prabhusaktivel@users.noreply.github.com>
2022-01-17 23:47:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var num = document .createElement("div")
+
+console.log(typeof(1))
+document.body.append(num)
+
+var num1 = document .createElement("div")
+
+console.log(typeof(1.1))
+document.body.append(num1)
+var num2 = document .createElement("div")
+
+console.log(typeof('1.1'))
+document.body.append(num2)
+var num3 = document .createElement("div")
+
+console.log(typeof(true))
+document.body.append(num3)
+var num4 = document .createElement("div")
+
+console.log(typeof(null))
+document.body.append(num4)
+var num5 = document .createElement("div")
+
+console.log(typeof(undefined))
+document.body.append(num5)
+var num6 = document .createElement("div")
+
+console.log(typeof([]))
+document.body.append(num6)
+var num7 = document .createElement("div")
+
+console.log(typeof({}))
+document.body.append(num7)
+var num8 = document .createElement("div")
+
+console.log(typeof(nan))
+document.body.append(num8)
